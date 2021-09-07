419fb45642bc219dd87e1b8fd69ba83f363f45f1
esonboyev03@bk.ru <esonboyev03@gmail.com>
2021-09-07 18:00:27 +0000

    [feat:] new feature added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+      <b>Learn React More and More</b>
         </a>
       </header>
     </div>
